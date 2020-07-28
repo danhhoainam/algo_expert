869bb5eb1f05dabb36a7c83fc1e8154c2e903998
Nguyen Danh Hoai Nam <nam.nguyenhoai@lazada.com>
2020-07-28 17:31:48 +0700

    python river size

---
+++ python/medium/prob_019_river_sizes/Solution.py
@@ -0,0 +1,51 @@
+def river_size(matrix):
+    visited = [[False for value in row] for row in matrix]
+    sizes = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if visited[i][j]:
+                continue
+            traverse(i, j, matrix, visited, sizes)
+    return sizes
+
+def traverse(i, j, matrix, visited, sizes):
+    currentRiverSize = 0
+    nodesToExplore = [[i, j]]
+    while len(nodesToExplore):
+        currentNode = nodesToExplore.pop()
+        i = currentNode[0]
+        j = currentNode[1]
+        if visited[i][j]:
+            continue
+        visited[i][j] = True
+        if matrix[i][j] == 0:
+            continue
+        currentRiverSize += 1
+        unvisitedNeighbors = get_unvisited_neighbors(i, j, matrix, visited)
+        for neighbor in unvisitedNeighbors:
+            nodesToExplore.append(neighbor)
+    if currentRiverSize > 0:
+        sizes.append(currentRiverSize)
+
+def get_unvisited_neighbors(i, j, matrix, visited):
+    unvisitedNeighbors = []
+    if i > 0 and not visited[i - 1][j]:
+        unvisitedNeighbors.append([i - 1, j])
+    if i < len(matrix) - 1 and not visited[i + 1][j]:
+        unvisitedNeighbors.append([i + 1, j])
+    if j > 0 and not visited[i][j - 1]:
+        unvisitedNeighbors.append([i, j - 1])
+    if j < len(matrix[0]) - 1 and not visited[i][j + 1]:
+        unvisitedNeighbors.append([i, j + 1])
+    return unvisitedNeighbors
+
+print(river_size([[0]]))
+print(river_size([[1]]))
+print(river_size([[1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0]]))
+print(river_size([
+    [1, 0, 0, 1, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0]
+  ]))
