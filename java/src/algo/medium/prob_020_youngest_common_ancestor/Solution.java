bad5c37f7d5770a858533b9a7fe5bf3dac2e8e34
Nguyen Danh Hoai Nam <nam.nguyenhoai@lazada.com>
2020-08-02 01:30:01 +0700

    youngest common ancestor

--- java/src/algo/medium/prob_020_youngest_common_ancestor/Solution.java
+++ java/src/algo/medium/prob_020_youngest_common_ancestor/Solution.java
@@ -1,5 +1,8 @@
 package algo.medium.prob_020_youngest_common_ancestor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public static void main(String[] args) {
