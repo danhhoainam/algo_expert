af42292819fb1071d35970808b2e92f18ec6400c
Nguyen Danh Hoai Nam <nam.nguyenhoai@lazada.com>
2020-10-05 22:35:58 +0700

    edit the imports

--- java/src/algo/medium/prob_020_youngest_common_ancestor/Solution.java
+++ java/src/algo/medium/prob_020_youngest_common_ancestor/Solution.java
@@ -1,8 +1,5 @@
 package algo.medium.prob_020_youngest_common_ancestor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
     public static void main(String[] args) {
